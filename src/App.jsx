a81efac94980ef9046a2afffbcb38576e9a9dded
AsemaniJoshua <joshuaasemani27@gmail.com>
2025-05-23 14:06:20 +0000

    Added transitions to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import Navbar from './Components/Navbar'
 import Hero from './Components/Home/Hero'
 import Relevant from './Components/Home/Relevant'
@@ -10,18 +12,21 @@ import Blog from './Components/Home/Blog'
 import Footer from './Components/Footer'
 
 function Home() {
-  
+  useEffect(() => {
+    AOS.init({ duration: 800, once: true });
+  }, []);
+
   return (
     <>
-      <Navbar />
-      <Hero />  
-      <Relevant />  
-      <Love />  
-      <Benefit />
-      <ChurchSermons />
-      <Serve />
-      <Blog />
-      <Footer />      
+      <Navbar data-aos="fade-down" />
+      <Hero data-aos="fade-up" />  
+      <Relevant data-aos="fade-right" />  
+      <Love data-aos="fade-left" />  
+      <Benefit data-aos="fade-up" />
+      <ChurchSermons data-aos="fade-right" />
+      <Serve data-aos="fade-left" />
+      <Blog data-aos="fade-up" />
+      <Footer data-aos="fade" />    
     </>
   )
 }
